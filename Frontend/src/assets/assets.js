b8cdf4c627e0d6d0d3084f0001b5dda8d3ccba4f
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-11-14 20:45:07 +0530

    create nav bar

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,4 +1,5 @@
 import basket_icon from './basket_icon.png'
+import cart_icon from './cart_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
@@ -63,6 +64,7 @@ import parcel_icon from './parcel_icon.png'
 export const assets = {
     logo,
     basket_icon,
+    cart_icon,
     header_img,
     search_icon,
     rating_starts,
