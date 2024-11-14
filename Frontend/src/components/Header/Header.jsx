b9cf5e9b087ebc496d59e37881b675bcd8689455
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-11-15 01:24:27 +0530

    create header page

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite pizza here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, quia
+          blanditiis laborum, at beatae suscipit corporis soluta porro
+          reprehenderit molestias repellendus reiciendis dignissimos totam,
+          magnam magni perferendis aliquam assumenda veritatis.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
