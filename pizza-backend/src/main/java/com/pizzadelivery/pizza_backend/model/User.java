8cf6aa6d4addfb847f3b4cfaa5bae8cf9060e937
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-12-19 00:28:22 +0530

    update

--- pizza-backend/src/main/java/com/pizzadelivery/pizza_backend/model/User.java
+++ pizza-backend/src/main/java/com/pizzadelivery/pizza_backend/model/User.java
@@ -9,7 +9,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Document(collection = "users")
 public class User {
-
     @Id
     private String id;
     private String userName;
