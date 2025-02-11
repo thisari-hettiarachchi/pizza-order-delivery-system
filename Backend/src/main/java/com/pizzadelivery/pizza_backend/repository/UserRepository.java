4acf399a5ea1d4b1fcedb58f9c75951099781b44
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-02-12 00:04:07 +0530

    update

--- Backend/src/main/java/com/pizzadelivery/pizza_backend/repository/UserRepository.java
+++ Backend/src/main/java/com/pizzadelivery/pizza_backend/repository/UserRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUserName(String userName);
-
 }
