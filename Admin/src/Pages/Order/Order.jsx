19276b468cd2ba29fe01789a5e8e40ade295a49e
Prabash Lakshitha <prabashlakshitha35@gmail.com>
2025-03-22 18:13:45 +0530

    Update Order.jsx

--- Admin/src/Pages/Order/Order.jsx
+++ Admin/src/Pages/Order/Order.jsx
@@ -49,12 +49,12 @@ const Orders = ({url}) => {
                     })
                     }
                 </p>
-                <p className="order-item-name">{order.address.firstName+""+order.address.lastName}</p>
+                <p className="order-item-name">{order.firstName+""+order.lastName}</p>
                 <div className="order-item-address">
                   <p>{order.address.street+","}</p>
                   <p>{order.address.city+","+order.address.state+","+order.address.country+","+order.address.zipcode}</p>
                 </div>
-                <p className='order-item-phone'>{order.address.phone}</p>
+                <p className='order-item-phone'>{order.contactNumber}</p>
               </div>
               <p>Item : {order.items.length}</p>
               <p>Rs.{order.amount}</p>
