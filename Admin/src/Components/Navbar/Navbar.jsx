f90ff2770e687a69003190cac5fb1848653ddd33
Rashmi Wickramarathne <wickramarathnerashmi9@gmail.com>
2025-03-22 16:55:09 +0530

    admin
    admin oder

--- Admin/src/Components/Navbar/Navbar.jsx
+++ Admin/src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbars = () => {
           <Container>
             <Navbar.Brand>
               <Link to="/" className="logo">
-                <img src={assets.logo} alt="Logo" className="img-fluid" />
+                <img src={assets.logo} alt="logo" className="img-fluid" />
               </Link>
             </Navbar.Brand>
             <Navbar.Collapse id="responsive-navbar-nav">
