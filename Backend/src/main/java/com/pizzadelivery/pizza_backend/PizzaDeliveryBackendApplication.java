c3d1c065b94258f22b749a3c2f06b7f078265cb5
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-06-01 15:20:01 +0530

    change stripe secret key and use .env file

--- Backend/src/main/java/com/pizzadelivery/pizza_backend/PizzaDeliveryBackendApplication.java
+++ Backend/src/main/java/com/pizzadelivery/pizza_backend/PizzaDeliveryBackendApplication.java
@@ -2,11 +2,14 @@ package com.pizzadelivery.pizza_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PizzaDeliveryBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("STRIPE_SECRET_KEY", dotenv.get("STRIPE_SECRET_KEY"));
 		SpringApplication.run(PizzaDeliveryBackendApplication.class, args);
 	}
 
